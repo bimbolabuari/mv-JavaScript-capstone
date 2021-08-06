4d6801a1e1e979aabbd2098aaabcd8d6c73b9368
Diego Ivan Solis Cuevas <diegoivansoliscuevas@Diegos-MacBook-Air.local>
2021-08-06 11:00:11 -0500

    fix show comments error

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../scss/style.scss';
 import { showsList } from './UX/cards';
 import showPopup from './UX/popup';
-import commentAPI from './API/Involvement';
+import commentAPI from './API/Comments';
 import newCounter from './UX/counter';
 
 showsList.renderCards();
